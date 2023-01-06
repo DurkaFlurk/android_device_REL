f2bfad94dd63fa8e3c1fe73e1fba3f43d2ae91d3
DurkaFlurk <113761843+DurkaFlurk@users.noreply.github.com>
2023-01-06 14:53:26 -0500

    Update Power_Armor14_Pro.mk

--- Power_Armor14_Pro.mk
+++ Power_Armor14_Pro.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := device/Ulefone/Power_Armor14_Pro
 
-$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
+$(shell mkdir -p $(OUT_DIR)/obj/KERNEL_OBJ/usr)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
